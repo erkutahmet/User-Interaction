f92126d089d6de5a65a05fedb49870c06e91765c
erkutahmet <90329328+erkutahmet@users.noreply.github.com>
2023-10-13 04:04:32 +0300

    Alert, Action Sheet, Custom Alert

--- User Interaction/User Interaction/ViewController.swift
+++ User Interaction/User Interaction/ViewController.swift
@@ -11,9 +11,69 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func alert(_ sender: Any) {
+        let alertController = UIAlertController(title: "Title", message: "Content", preferredStyle: .alert)
+        
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel) { action in
+            print("Cancel selected!")
+        }
+        
+        alertController.addAction(cancelButton)
+        
+        let okayButton = UIAlertAction(title: "Okay", style: .destructive) { action in
+            print("Okay selected!")
+        }
+        
+        alertController.addAction(okayButton)
+        
+        self.present(alertController, animated: true)
+    }
+    
+    @IBAction func actionSheet(_ sender: Any) {
+        let alertController = UIAlertController(title: "Title", message: "Content", preferredStyle: .actionSheet)
+        
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel) { action in
+            print("Cancel selected!")
+        }
+        
+        alertController.addAction(cancelButton)
+        
+        let okayButton = UIAlertAction(title: "Okay", style: .destructive) { action in
+            print("Okay selected!")
+        }
+        
+        alertController.addAction(okayButton)
+        
+        self.present(alertController, animated: true)
+    }
+    
+    @IBAction func customAlert(_ sender: Any) {
+        let alertController = UIAlertController(title: "Title", message: "Content", preferredStyle: .alert)
+        
+        alertController.addTextField() // 0
+        
+        alertController.addTextField() { textField in // 1
+            textField.placeholder = "Enter data"
+            textField.keyboardType = .numberPad
+            textField.isSecureTextEntry = true
+        }
+        
+        let saveButton = UIAlertAction(title: "Save", style: .default) { action in
+            let tf = alertController.textFields![0] as UITextField
+            let tf1 = alertController.textFields![1] as UITextField
+            
+            if let gotData = tf.text, let gotData1 = tf1.text {
+                print("Data: \(gotData)")
+                print("Data1: \(gotData1)")
+            }
+            
+        }
+        
+        alertController.addAction(saveButton)
+        
+        self.present(alertController, animated: true)
+    }
 }
 
